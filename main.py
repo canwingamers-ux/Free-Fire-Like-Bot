d484bfe4ab5c0bfc8accd2b933e92d921ccf2126
canwingamers-ux <canwingamers@gmail.com>
2026-05-05 23:00:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # === CONFIG ===
-BOT_TOKEN = os.getenv("8729928121:AAGtl_bbh94IHrxhA51xOv2UHV7q8fVoi_k")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 if not BOT_TOKEN:
     logger.error("❌ BOT_TOKEN not found! Please set your bot token in environment variables.")
